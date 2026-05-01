c943d591513c1b0f26e61487b337fca42c2b3167
santhosh <santhoshkarur13@gmail.com>
2026-05-01 14:16:50 +0530

    fix: added dotenv dependency

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ app.use('/api/borrow', borrowRoutes);
 app.use('/api/fines', fineRoutes);
 app.use('/api/members', memberRoutes);
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/library_system';
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
